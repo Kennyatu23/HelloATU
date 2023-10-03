fe6828c9b3c08b63e01da104240083b784d062a0
kennyatu23 <g00267173@atu.ie>
2023-10-03 16:56:25 +0100

    NameB

--- src/main/java/ie/atu/demo/HelloController.java
+++ src/main/java/ie/atu/demo/HelloController.java
@@ -2,6 +2,7 @@ package ie.atu.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloController {
@@ -10,5 +11,10 @@ public class HelloController {
     public  String hello(){
         return "Hello, World!";
     }
+    @GetMapping("/greet/{name}")
+    public  String greetByName(@PathVariable String name){
+        return "Hello" +name;
+    }
+
 
 }
